32f192d69007013590e1952125dbd9f54b32c011
Tntragan <tomandtammyragan@gmail.com>
2024-06-29 21:48:43 -0700

    Feat: add spell changer

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,40 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    checkSpelling(sentence) {
+        const sentenceArray = sentence.split(/(\b)/g);
+        // console.log(sentenceArray);
+        // console.log(sentenceArray.join(''));
+        //console.log(sentenceArray);
+        for (let i = 0; i < sentenceArray.length; i++) {
+            for (let key in americanToBritishSpelling) {
+                const word = sentenceArray[i];
+                if (word.toLowerCase() == key) {
+                    sentenceArray[i] = americanToBritishSpelling[key];
+                }
+            }
+
+        }
+        const newSentence = sentenceArray.join('');
+        console.log(sentence);
+        console.log(this.capFirstLetter(newSentence));
+    }
+
+    capFirstLetter(sentence) {
+        const sentenceArray = sentence.split(/[!.?]/g)
+        let newArray = [];
+        for (let i = 0; i < sentenceArray.length; i++) {
+            if (sentenceArray[i].charAt(0) == " ") {
+                newArray.push(sentenceArray[i].charAt(1).toUpperCase() + sentenceArray[i].slice(2));
+            } else {
+                newArray.push(sentenceArray[i].charAt(0).toUpperCase() + sentenceArray[i].slice(1));
+            }
+        }
+        return newArray.join('');
+        //console.log((sentence.charAt(0).toUpperCase() + sentence.slice(1)));
+        //console.log(newArray);
+    }
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,14 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const sentence = req.body.text;
+      const spellWords = translator.checkSpelling(sentence);
+      const capitalize = translator.capFirstLetter(sentence);
+
     });
 };
